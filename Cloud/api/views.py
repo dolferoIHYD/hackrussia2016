0ef8b2c0d106a65f759e75c28891afb9945f0519
Boris Paschenko <i_hope-you_die@mail.ru>
2016-11-19 10:55:37 +0300

    removed serializers

--- Cloud/api/views.py
+++ Cloud/api/views.py
@@ -2,7 +2,6 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from api.serializers import TruckSerializer, ArrivalSerializer, DeportSerializer
 from tracker.models import Truck, Arrival, Deport
 
 @api_view(['GET'])
@@ -12,17 +11,14 @@ def receive(request):
         truck = Truck.objects.get(token=data.get('token', default=None))
     except Truck.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-    # data  = {'truck':truck}
     if truck.at_poligon:
         event = Deport.objects.create(truck=truck)
         truck.at_poligon = False
         truck.save()
-        # serializer = DeportSerializer(data=data)
     else:
         event = Arrival.objects.create(truck=truck)
         truck.at_poligon = True
         truck.save()
-        # serializer = ArrivalSerializer(data=data)
 
     print ' # Event created: ' + str(event.time)
     return Response(status=status.HTTP_201_CREATED)
